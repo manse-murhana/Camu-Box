b48196aa252ce014101a20d96dc96f5836eeb32f
22mSqRi <37729945+22mSqRi@users.noreply.github.com>
2026-04-11 14:29:05 +0900

    fix: 参照エラー修正

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,5 @@
+import "./polyfills";
+
 import { createApp } from "vue";
 import { createPinia } from "pinia";
 
--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,21 @@ import basicSsl from "@vitejs/plugin-basic-ssl";
 export default defineConfig(({ mode }) => ({
   plugins: [vue(), basicSsl()],
   base: mode === "production" ? "/Camu-Box/" : "/",
+  define: {
+    global: "globalThis",
+  },
+  resolve: {
+    alias: {
+      buffer: "buffer",
+    },
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      define: {
+        global: "globalThis",
+      },
+    },
+  },
   build: {
     outDir: "dist",
     assetsDir: "assets",
